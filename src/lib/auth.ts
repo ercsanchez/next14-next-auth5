0a6cee38cd0ed622a53fedfb3f4078ab86b009a5
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2024-05-03 23:40:52 +0800

    feat: 10) 02:06:30 - initial auth setup

---
+++ src/lib/auth.ts
@@ -0,0 +1,14 @@
+// NOTE: in the tutorial, this file is located in the root of the project (if not using src) or in the root of src (if using src)
+
+import NextAuth from "next-auth";
+import Github from "next-auth/providers/github";
+
+export const {
+  handlers, // auth.js docs
+  // handlers: { GET, POST }, // tutorial
+  auth,
+  // signIn, // auth.js docs
+  // signOut, // auth.js docs
+} = NextAuth({
+  providers: [Github],
+});
