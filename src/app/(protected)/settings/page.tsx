113d3287569529c4a88160ce33e5f8beac344bbb
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2024-05-09 21:37:47 +0800

    feat: 21) 06:58:20 - add settings page update form for name change

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -2,18 +2,59 @@
 
 import { useTransition } from "react";
 import { useSession } from "next-auth/react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
+import { useCurrentUser } from "~/hooks/use-current-user";
+import { SettingsSchema } from "~/schemas";
+import { settings } from "~/actions/settings";
 import { Button } from "~/components/ui/button";
 import { Card, CardHeader, CardContent } from "~/components/ui/card";
-import { settings } from "~/actions/settings";
+import {
+  Form,
+  FormField,
+  FormControl,
+  FormItem,
+  FormLabel,
+  FormDescription,
+  FormMessage,
+} from "~/components/ui/form";
+import { FormSuccess } from "~/components/form-success";
+import { FormError } from "~/components/form-error";
+import { Input } from "~/components/ui/input";
 
 export default function SettingsPage() {
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
   const { update } = useSession();
   const [isPending, startTransition] = useTransition();
 
-  const onClick = () => {
+  const user = useCurrentUser();
+
+  const form = useForm<z.infer<typeof SettingsSchema>>({
+    resolver: zodResolver(SettingsSchema),
+    defaultValues: {
+      name: user?.name || undefined, // don't use "" since empty string will be written to the database, while undefined will not, because of spread operator on {...values} (see actions/settings.ts)
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof SettingsSchema>) => {
     startTransition(() => {
-      settings({ name: "New Name!" }).then(() => update());
+      settings(values)
+        .then((data) => {
+          if (data.error) {
+            setError(data.error);
+          }
+          if (data.success) {
+            update();
+            setSuccess(data.success);
+          }
+        })
+        .catch(() => {
+          setError("Something went wrong!");
+        });
     });
   };
 
@@ -22,10 +63,35 @@ export default function SettingsPage() {
       <CardHeader>
         <p className="text-center text-2xl font-semibold">⚙ Settings</p>
       </CardHeader>
-      <CardContent className="space-y-4">
-        <Button onClick={onClick} disabled={isPending}>
-          Update name
-        </Button>
+      <CardContent>
+        <Form {...form}>
+          <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="space-y-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        // placeholder={user.name || ""}
+                        placeholder={"John Doe"}
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <FormError message={error} />
+            <FormSuccess message={success} />
+            <Button type="submit" disabled={isPending}>
+              Save
+            </Button>
+          </form>
+        </Form>
       </CardContent>
     </Card>
   );
