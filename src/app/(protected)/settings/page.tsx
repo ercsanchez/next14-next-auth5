ea3808242933b4d1e44c24ccc1f2d22c0ee441b1
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2024-05-10 20:55:22 +0800

    feat: 21) 07:06:46 - add ui for user to change email, password; user to select role; user to toggle two-factor auth - backend functionality not adjusted yet to restrict user updates on the User model - ui changes only for this commit

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -10,6 +10,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useCurrentUser } from "~/hooks/use-current-user";
 import { SettingsSchema } from "~/schemas";
 import { settings } from "~/actions/settings";
+import { UserRole } from "@prisma/client";
 import { Button } from "~/components/ui/button";
 import { Card, CardHeader, CardContent } from "~/components/ui/card";
 import {
@@ -24,6 +25,14 @@ import {
 import { FormSuccess } from "~/components/form-success";
 import { FormError } from "~/components/form-error";
 import { Input } from "~/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
+import { Switch } from "~/components/ui/switch";
 
 export default function SettingsPage() {
   const [error, setError] = useState<string | undefined>();
@@ -37,6 +46,11 @@ export default function SettingsPage() {
     resolver: zodResolver(SettingsSchema),
     defaultValues: {
       name: user?.name || undefined, // don't use "" since empty string will be written to the database, while undefined will not, because of spread operator on {...values} (see actions/settings.ts)
+      email: user?.email || undefined,
+      password: undefined,
+      newPassword: undefined,
+      role: user?.role || undefined,
+      isTwoFactorEnabled: user?.isTwoFactorEnabled || undefined,
     },
   });
 
@@ -81,9 +95,123 @@ export default function SettingsPage() {
                         disabled={isPending}
                       />
                     </FormControl>
+                    <FormMessage />
                   </FormItem>
                 )}
               />
+              {/* ONLY SHOW IF USER DID NOT REGISTER WITH OAUTH PROVIDER (USER USED CREDENTIALS) */}
+              {/* 2-factor auth is only avail for users who registered with credentials & user should not be allowed to change email (references oauth email) and password (null) if they registered with oauth because these were set by next-auth and changes will break oauth sign-in */}
+              {/* tutorial used "user?.isOAuth === false" but this is shorter and works the same */}
+              {!user?.isOAuth && (
+                <>
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            // placeholder={user.name || ""}
+                            placeholder={"john.doe@mail.com"}
+                            disabled={isPending}
+                            type="email"
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            // placeholder={user.name || ""}
+                            placeholder={"******"}
+                            disabled={isPending}
+                            type="password"
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="newPassword"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>New Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            // placeholder={user.name || ""}
+                            placeholder={"******"}
+                            disabled={isPending}
+                            type="password"
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </>
+              )}
+              <FormField
+                control={form.control}
+                name="role"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Role</FormLabel>
+                    <Select
+                      disabled={isPending}
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a role" />
+                        </SelectTrigger>
+                      </FormControl>
+
+                      <SelectContent>
+                        <SelectItem value={UserRole.ADMIN}>Admin</SelectItem>
+                        <SelectItem value={UserRole.USER}>User</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {!user?.isOAuth && (
+                <FormField
+                  control={form.control}
+                  name="isTwoFactorEnabled"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+                      <div className="space-y-0.5">
+                        <FormLabel>Two Factor Authentication</FormLabel>
+                        <FormDescription>
+                          Enable two factor authentication for your account
+                        </FormDescription>
+                      </div>
+                      <FormControl>
+                        <Switch
+                          disabled={isPending}
+                          checked={field.value}
+                          onCheckedChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              )}
             </div>
             <FormError message={error} />
             <FormSuccess message={success} />
