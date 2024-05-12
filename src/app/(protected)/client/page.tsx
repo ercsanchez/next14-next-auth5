93bba41931c9b6303277589869049803bf665ad9
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2024-05-12 15:02:12 +0800

    fix: pass session user as prop to client components (/settings & /client), since session user is undefined on initial login

--- src/app/(protected)/client/page.tsx
+++ src/app/(protected)/client/page.tsx
@@ -1,10 +1,12 @@
-"use client";
+// "use client";
 
-import { useCurrentUser } from "~/hooks/use-current-user";
+// import { useCurrentUser } from "~/hooks/use-current-user";
+import { currentUser } from "~/lib/auth";
 import { UserInfo } from "~/components/user-info";
 
-export default function ClientPage() {
-  const user = useCurrentUser();
+export default async function ClientPage() {
+  // const user = useCurrentUser();
+  const user = await currentUser();
 
   return <UserInfo user={user} label="👩 Client component" />;
 }
